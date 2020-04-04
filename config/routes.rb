de0ad7e493829c98b31d0fde7b4730f8ff8ecfdc
AimableHonorable <aimatwiri@gmail.com>
2020-04-04 22:12:46 +0200

    homepage design

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :payments
   resources :replies
+  resources :homepage
   get 'comments/create'
   get 'course/assesment'
   get 'welcome/index'
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'professional', to: 'users#professional', as: 'professional'
   get 'instructor', to: 'users#instructor', as: 'instructor'
-  
+
   devise_for :users, controllers: {
     registrations: "users/registrations",
     # Add this
@@ -19,7 +20,7 @@ Rails.application.routes.draw do
 resources :courses do
   resources :comments
 end
-  
+
   # devise_for :users
   root to: "courses#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
