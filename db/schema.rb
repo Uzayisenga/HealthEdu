7e2b0329f66b0733fd9b0a1013757d446019f889
= <=>
2020-05-15 11:55:05 +0200

    user notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_004155) do
+ActiveRecord::Schema.define(version: 2020_05_11_083113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,36 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "choice_id", null: false
+    t.bigint "question_id", null: false
+    t.bigint "response_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["choice_id"], name: "index_answers_on_choice_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["response_id"], name: "index_answers_on_response_id"
+  end
+
+  create_table "choices", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_choices_on_question_id"
+  end
+
+  create_table "collaborations", force: :cascade do |t|
+    t.bigint "survey_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id"], name: "index_collaborations_on_survey_id"
+    t.index ["user_id"], name: "index_collaborations_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -97,27 +127,20 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.index ["quiz_id"], name: "index_mc_questions_on_quiz_id"
   end
 
-  create_table "notifications", force: :cascade do |t|
-    t.string "target_type", null: false
-    t.bigint "target_id", null: false
-    t.string "notifiable_type", null: false
-    t.bigint "notifiable_id", null: false
-    t.string "key", null: false
-    t.string "group_type"
-    t.bigint "group_id"
-    t.integer "group_owner_id"
-    t.string "notifier_type"
-    t.bigint "notifier_id"
-    t.text "parameters"
-    t.datetime "opened_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.index ["group_owner_id"], name: "index_notifications_on_group_owner_id"
-    t.index ["group_type", "group_id"], name: "index_notifications_on_group_type_and_group_id"
-    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable_type_and_notifiable_id"
-    t.index ["notifier_type", "notifier_id"], name: "index_notifications_on_notifier_type_and_notifier_id"
-    t.index ["target_type", "target_id"], name: "index_notifications_on_target_type_and_target_id"
+  create_table "notifications", id: :serial, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "actor_id"
+    t.string "notify_type", null: false
+    t.string "target_type"
+    t.integer "target_id"
+    t.string "second_target_type"
+    t.integer "second_target_id"
+    t.string "third_target_type"
+    t.integer "third_target_id"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "notify_type"], name: "index_notifications_on_user_id_and_notify_type"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
@@ -139,6 +162,15 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.integer "type"
+    t.bigint "survey_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id"], name: "index_questions_on_survey_id"
+  end
+
   create_table "quizzes", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -146,6 +178,49 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_quizzes_on_course_id"
+  end
+
+  create_table "rapidfire_answers", force: :cascade do |t|
+    t.bigint "attempt_id"
+    t.bigint "question_id"
+    t.text "answer_text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["attempt_id"], name: "index_rapidfire_answers_on_attempt_id"
+    t.index ["question_id"], name: "index_rapidfire_answers_on_question_id"
+  end
+
+  create_table "rapidfire_attempts", force: :cascade do |t|
+    t.bigint "survey_id"
+    t.string "user_type"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id"], name: "index_rapidfire_attempts_on_survey_id"
+    t.index ["user_id", "user_type"], name: "index_rapidfire_attempts_on_user_id_and_user_type"
+    t.index ["user_type", "user_id"], name: "index_rapidfire_attempts_on_user_type_and_user_id"
+  end
+
+  create_table "rapidfire_questions", force: :cascade do |t|
+    t.bigint "survey_id"
+    t.string "type"
+    t.string "question_text"
+    t.string "default_text"
+    t.string "placeholder"
+    t.integer "position"
+    t.text "answer_options"
+    t.text "validation_rules"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id"], name: "index_rapidfire_questions_on_survey_id"
+  end
+
+  create_table "rapidfire_surveys", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "after_survey_content"
   end
 
   create_table "replies", force: :cascade do |t|
@@ -165,6 +240,14 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["course_id"], name: "index_requests_on_course_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.bigint "survey_id", null: false
+    t.string "ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["survey_id"], name: "index_responses_on_survey_id"
   end
 
   create_table "results", force: :cascade do |t|
@@ -198,6 +281,14 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.index ["target_type", "target_id"], name: "index_subscriptions_on_target_type_and_target_id"
   end
 
+  create_table "surveys", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "course_id", null: false
+    t.index ["course_id"], name: "index_surveys_on_course_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -211,12 +302,12 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
     t.string "gender"
     t.string "phone"
     t.string "district"
-    t.integer "reguratory_body"
-    t.integer "level"
+    t.string "reguratory_body"
+    t.string "level"
     t.string "user_role", default: "professional"
     t.string "apload_diploma"
     t.string "apload_cv"
-    t.integer "working_place"
+    t.string "working_place"
     t.string "uid", default: "", null: false
     t.string "provider", default: "", null: false
     t.string "last_name"
@@ -227,6 +318,12 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "choices"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "responses"
+  add_foreign_key "choices", "questions"
+  add_foreign_key "collaborations", "surveys"
+  add_foreign_key "collaborations", "users"
   add_foreign_key "comments", "courses"
   add_foreign_key "comments", "users"
   add_foreign_key "courses", "users"
@@ -234,14 +331,16 @@ ActiveRecord::Schema.define(version: 2020_05_15_004155) do
   add_foreign_key "favorites", "courses"
   add_foreign_key "favorites", "users"
   add_foreign_key "mc_questions", "quizzes"
-  add_foreign_key "notifications", "users"
   add_foreign_key "payments", "courses"
   add_foreign_key "payments", "users"
   add_foreign_key "profiles", "users"
+  add_foreign_key "questions", "surveys"
   add_foreign_key "quizzes", "courses"
   add_foreign_key "replies", "comments"
   add_foreign_key "replies", "users"
   add_foreign_key "requests", "courses"
   add_foreign_key "requests", "users"
+  add_foreign_key "responses", "surveys"
   add_foreign_key "results", "mc_questions"
+  add_foreign_key "surveys", "courses"
 end
