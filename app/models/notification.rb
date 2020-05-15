316d90e4349fb049c7751cd6ee0cf03200db5b4a
Uzayisenga <aminauzayisenga@gmail.com>
2020-05-15 02:24:23 +0200

    make notification appear

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,7 @@
 # Auto generate with notifications gem.
 class Notification < ActiveRecord::Base
   include Notifications::Model
-
+  validates :user_id, presence: true
+  has_one :user, class_name: "User", :foreign_key => "user_id" 
   # Write your custom methods...
 end
