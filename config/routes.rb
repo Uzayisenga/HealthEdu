974dd4f50343b5a553e0a7798f2bf47788fc9fe8
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-06-04 17:31:37 +0200

    Update routes.rb
    Logout doesn't work.

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,13 @@ Rails.application.routes.draw do
   get 'instructor', to: 'users#instructor', as: 'instructor'
   get 'search', to: 'users#search', as: :search
   get 'quiz', to: 'users#quiz', as: 'quiz'
- get 'exit', to: 'sessions#destroy', as: :logout
+ 
 
-  
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    # Add this
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :users, :only =>[:show, :index]
 resources :courses do
   collection do
