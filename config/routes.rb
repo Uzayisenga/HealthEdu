a12b0b59828336994abef2d26cc618077129ffd5
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-06-08 13:55:39 +0200

    Update routes.rb
    post "/logout" => "devise/sessions#destroy", :as => :destroy_user_session

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get 'instructor', to: 'users#instructor', as: 'instructor'
   get 'search', to: 'users#search', as: :search
   get 'quiz', to: 'users#quiz', as: 'quiz'
-
+  post "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
   devise_for :users, controllers: {
     registrations: "users/registrations",
     # Add this
