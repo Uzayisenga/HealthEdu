a0c6bd8da3c38db3f963c836903c041ea5e60cd3
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-06-08 15:14:26 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,7 @@ Rails.application.routes.draw do
   get 'instructor', to: 'users#instructor', as: 'instructor'
   get 'search', to: 'users#search', as: :search
   get 'quiz', to: 'users#quiz', as: 'quiz'
-  devise_for :users, controllers: {
-    registrations: "users/registrations",
-    # Add this
-    omniauth_callbacks: "users/omniauth_callbacks"
-  }
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' 
 
   resources :users, :only =>[:show, :index]
 resources :courses do
