63c22f52f7b431996a70e831cdd27df27df9b308
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-06-09 11:15:55 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ resources :courses do
   end
 end
 
-  # devise_for :users
+  
   root to: "homepage#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
