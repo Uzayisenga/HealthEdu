58e3ee1be7ec527a3a02e7403a919c7ec370a8db
nadinantire <53227502+nadinantire@users.noreply.github.com>
2020-06-13 11:01:43 +0200

    Update favorites_controller.rb

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,9 @@
 class FavoritesController < ApplicationController
 before_action :can_request_course, only: [:create]
-  def index
+  def create
     @favorites=Favorite.all
   end
-  def create
+  def index
     favorite = current_user.favorites.create(course_id: params[:course_id])
     redirect_to homepage_index_url, notice: "You have request this course, Please make payment on Bank of Kigali Bank Account: 00262-06949899-22 / Top-Upgrade Ltd or use MTN Mobile Money on 0788848750 registered to Uwandamiye Emmanuella or 0730300074 registered to Jean Damascene BIGIRIMANA. In case you have a question, contact of of those phone numbers or e-mail: healthedultd@gmail.com "
   end
