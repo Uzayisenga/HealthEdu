71b164ef70c1c50e6efc36ff548db0e94c904ab2
nadinantire <nadinantire@gmail.com>
2020-06-17 02:09:27 +0200

    design

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get 'certificate/index'
   get 'requests/update'
   resources :credits
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  authenticate :user, lambda { |u| u.user_role == "admin" } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
   resources :replies
   resources :homepage
   resources :favorites, only: [:index, :destroy, :create, :show]
@@ -20,6 +22,7 @@ Rails.application.routes.draw do
   get 'professional', to: 'users#professional', as: 'professional'
   get 'report', to: 'favorites#report', as: 'report'
   get 'all_favorite', to: 'favorites#all_favorite', as: 'all_favorite'
+  get 'single', to: 'favorites#single', as: 'single'
   get 'instructor', to: 'users#instructor', as: 'instructor'
   get 'search', to: 'users#search', as: :search
   get 'quiz', to: 'users#quiz', as: 'quiz'
