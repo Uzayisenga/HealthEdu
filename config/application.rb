69a1d552ff52b6c124d775dabdd206905ba60e31
nadinantire <53227502+nadinantire@users.noreply.github.com>
2020-07-11 22:47:36 +0200

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module HealthEdu
     config.load_defaults 6.0
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
+    config.session_store :cookie_store
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use ::Rack::MethodOverride
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
