984bb1d493bad7a83adf51367c4dd171ca146aa4
nadinantire <nadinantire@gmail.com>
2020-07-31 19:21:46 +0200

    logo

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,7 +6,7 @@ def all_favorite
   end
   def index
     favorite = current_user.favorites.create(course_id: params[:course_id])
-    redirect_to homepage_index_url, notice: "You have request this course, Please make payment on Bank of Kigali Bank Account: 00262-06949899-22 / Top-Upgrade Ltd or use MTN Mobile Money on 0788848750 registered to Uwandamiye Emmanuella or contact to this e-mail: healthedultd@gmail.com "
+    redirect_to homepage_index_url, notice: "You have requested this course, Please make your payment at Bank of Kigali Account: 00262-06949899-22 / HealthEdu Ltd or use MTN Mobile Money on 0788848750 registered to Uwandamiye Emmanuella or Momo Pay: 000602. Contact us to this e-mail: healthedultd@gmail.com or Tel.: 0789044439."
   end
 
 def show
