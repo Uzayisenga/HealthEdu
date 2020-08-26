67687a942d07d1bc5f00098b81f99ba80b511010
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-08-26 17:08:18 +0200

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.config.hosts << "www.healtheducat.rw"
+Rails.application.config.hosts = nill
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
