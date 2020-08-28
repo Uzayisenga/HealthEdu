535196a930b112bcbf5d4225785b03854db99b09
Uzayisenga <53423820+Uzayisenga@users.noreply.github.com>
2020-08-28 11:34:09 +0200

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Rails.application.config.hosts = nill
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
