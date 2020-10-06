e5db0cfb797ff6888e430c58fce2d1b31815d10e
mygithubangebenie <angebenie23@gmail.com>
2020-10-06 15:13:54 +0200

    pushing error page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_125543) do
+ActiveRecord::Schema.define(version: 2020_09_29_110057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_10_06_125543) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  
   create_table "books", force: :cascade do |t|
     t.string "book_name"
     t.string "author_name"
